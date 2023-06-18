f896d5b060919a0b3b9fa4289af30ea387b34827
Jason Woodgate <jaskian1@gmail.com>
2023-06-18 12:48:41 +0100

    data puller now working

--- tmb-puller/main.go
+++ tmb-puller/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io/fs"
 	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/caarlos0/env"
@@ -12,7 +14,6 @@ import (
 )
 
 const Guild_Slug = "https://thatsmybis.com/9044/reset/"
-const tmbAuthLink = "https://thatsmybis.com/auth/discord"
 
 type config struct {
 	DiscordUsername string `env:"DISCORD_USERNAME"`
@@ -20,59 +21,54 @@ type config struct {
 }
 
 func main() {
+	// load env
 	cfg := config{}
 	if err := env.Parse(&cfg); err != nil {
 		fmt.Printf("%+v\n", err)
 	}
 
+	// create go-rod launcher
 	l := launcher.New().
 		Headless(false).
 		Devtools(false)
+	defer l.Cleanup()
 
-	defer l.Cleanup() // remove launcher.FlagUserDataDir
-
+	// create browser
 	url := l.MustLaunch()
-
-	// Trace shows verbose debug information for each action executed
-	// SlowMotion is a debug related function that waits 2 seconds between
-	// each action, making it easier to inspect what your code is doing.
 	browser := rod.New().
 		ControlURL(url).
 		SlowMotion(time.Millisecond * 45).
 		MustConnect()
-
 	defer browser.MustClose()
-	// ServeMonitor plays screenshots of each tab. This feature is extremely
-	// useful when debugging with headless mode.
-	// You can also enable it with flag "-rod=monitor"
-	//launcher.Open(browser.ServeMonitor(""))
 
+	// login
 	page := browser.MustPage("https://thatsmybis.com/auth/discord")
-
 	page.MustElement("input[name=\"email\"]").MustInput(cfg.DiscordUsername)
 	page.MustElement("input[name=\"password\"]").MustInput(cfg.DiscordPassword)
-
 	page.MustElement("button[type=\"submit\"]").Click(proto.InputMouseButtonLeft, 1)
+	time.Sleep(time.Second * 5)
 
-	selector := fmt.Sprintf("a[href=\"%s\"]", tmbAuthLink)
-	if has, element, err := page.HasX(selector); has {
-		element.Click(proto.InputMouseButtonLeft, 1)
-		time.Sleep(time.Second * 5)
-	} else if err != nil {
-		panic(err)
-	}
+	// create temp dir and set as download location
+	wd, _ := os.Getwd()
+	tempDir, _ := os.MkdirTemp(wd, "tmp")
+	defer func() {
+		os.RemoveAll(tempDir)
+	}()
+	// only using this to set the download location, the wait wont work
+	browser.WaitDownload(tempDir)
 
-	page = browser.MustPage(Guild_Slug + "export")
+	// download big json blob
+	page.Navigate(Guild_Slug + "export")
+	downloadUrl := Guild_Slug + "export/characters-with-items/json"
+	selector := fmt.Sprintf("a[href=\"%s\"]", downloadUrl)
+	page.MustElement(selector).MustClick()
+	time.Sleep(time.Second * 5)
 
-	wait := page.Browser().MustWaitDownload()
-
-	downloadUrl := Guild_Slug + "export/characters-with-items/html"
-	page.MustElement(fmt.Sprintf("a[href=\"%s\"]", downloadUrl)).Click(proto.InputMouseButtonLeft, 1)
-
-	res := wait()
-	err := os.WriteFile("../app/data/character-json1.json", res, 0644)
-
-	if err != nil {
-		panic(err)
-	}
+	// move to the right place
+	filepath.WalkDir(tempDir, func(s string, d fs.DirEntry, e error) error {
+		if !d.IsDir() {
+			os.Rename(s, "../app/data/character-json.json")
+		}
+		return nil
+	})
 }
